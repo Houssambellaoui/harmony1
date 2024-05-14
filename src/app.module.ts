336acbe369606ef00c2e4dc8ff759664930d275f
houssam <39628814+HoussamBL@users.noreply.github.com>
2024-05-14 16:57:24 +0100

    Test app

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './books/books.module';
+
+@Module({
+  imports: [  
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    BooksModule],
+  
+})
+export class AppModule {}
